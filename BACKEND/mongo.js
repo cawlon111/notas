7afa8bd301e0683968c4ec4f73131ebe9baa8150
cawlon111 <cawlon111@gmail.com>
2026-05-04 12:30:21 +0200

    mongo terminado

--- BACKEND/mongo.js
+++ BACKEND/mongo.js
@@ -1,32 +1,31 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
-  console.log('give password as argument')
-  process.exit(1)
-}
-
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://cawlon:${password}@cluster0.9caici4.mongodb.net/?appName=Cluster0`
+  `mongodb://cawlon:${password}@ac-wghbgrm-shard-00-00.9caici4.mongodb.net:27017,ac-wghbgrm-shard-00-01.9caici4.mongodb.net:27017,ac-wghbgrm-shard-00-02.9caici4.mongodb.net:27017/noteApp?ssl=true&replicaSet=atlas-sm1n4b-shard-0&authSource=admin&appName=Cluster0`
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 
-mongoose.connect(url)
-
+// Schema
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
 })
 
+// Model
 const Note = mongoose.model('Note', noteSchema)
 
-const note = new Note({
-  content: 'HTML is easy',
-  important: true,
-})
+// Conexión
+mongoose.connect(url)
 
-note.save().then(result => {
-  console.log('note saved!')
+// Obtener todas las notas
+Note.find({}).then(result => {
+  console.log('notes:')
+
+  result.forEach(note => {
+    console.log(note)
+  })
+
   mongoose.connection.close()
 })
